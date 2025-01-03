65f6117dd6c64ab96dd9fc4bcc9c31a8664365a6
heroband <dyalist2001@gmail.com>
2025-01-03 15:48:47 +0200

    Добавьте файлы проекта.

---
+++ ASP CORE MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_CORE_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
