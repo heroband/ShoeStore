c5272096b685ccfe81fd1c8c5df59d74b6aac0fe
heroband <dyalist2001@gmail.com>
2025-01-03 17:48:31 +0200

    added sneakers creating page and sneakers model

---
+++ ASP CORE MVC/Controllers/AddController.cs
@@ -0,0 +1,24 @@
+﻿using ASP_CORE_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_CORE_MVC.Controllers
+{
+    public class AddController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Sneakers sneakers)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ ASP CORE MVC/Models/Sneakers.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace ASP_CORE_MVC.Models
+{
+    public class Sneakers
+    {
+        [Display(Name = "Input sneakers name")]
+        [Required(ErrorMessage = "You need to input name")]
+        public string? Name { get; set; }
+        [Display(Name = "Input sneakers description")]
+        [Required(ErrorMessage = "You need to input description")]
+        public string? Description { get; set; }
+        [Display(Name = "Input sneakers id")]
+        [Required(ErrorMessage = "You need to input id")]
+        public string Id { get; set; }
+        [Display(Name = "Input sneakers size")]
+        [Required(ErrorMessage = "You need to input size")]
+        [Range(1, 99, ErrorMessage = "Size must be between 1 and 99.")]
+        public int Size { get; set; }
+        [Display(Name = "Input sneakers price")]
+        [Required(ErrorMessage = "You need to input price")]
+        public decimal Price { get; set; }
+
+    }
+}
