135e13332ff90542e115a14e59e32c65af32b498
heroband <dyalist2001@gmail.com>
2025-01-08 14:55:02 +0200

    added authorization and changed header

--- ShoeStore/Controllers/AccountController.cs
+++ ShoeStore/Controllers/AccountController.cs
@@ -11,11 +11,46 @@ namespace ShoeStore.Controllers
     public class AccountController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
-        public AccountController(IUserRepository userRepository, SignInManager<User> signInManager)
+        public AccountController(IUserRepository userRepository, UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _userRepository = userRepository;
+            _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+            
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "No account registered with this email.");
+                return View(loginViewModel);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.Email, loginViewModel.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("Password", "Invalid password.");
+                return View(loginViewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
 
 
@@ -49,6 +84,13 @@ namespace ShoeStore.Controllers
             await _userRepository.AddUserAsync(user, userViewModel.Password);
             await _signInManager.SignInAsync(user, isPersistent: false);
             
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
     }
